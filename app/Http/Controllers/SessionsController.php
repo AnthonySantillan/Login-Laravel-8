08ccd11beb8c7fdf2892c8781457652d3a1d025d
Anthony Santillan <58825544+AnthonySantillan@users.noreply.github.com>
2022-03-19 15:13:29 -0500

    login

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class SessionsController extends Controller {
+    
+    public function create() {
+        
+        return view('auth.login');
+    }
+
+    public function store() {
+        
+        if(auth()->attempt(request(['email', 'password'])) == false) {
+            return back()->withErrors([
+                'message' => 'EL correo o contraseña son incorrectas, ingrese de nuevo por favor',
+            ]);
+
+        } else {
+
+            if(auth()->user()->role == 'admin') {
+                return redirect()->route('admin.index');
+            } else {
+                return redirect()->to('/');
+            }
+        }
+    }
+
+    public function destroy() {
+
+        auth()->logout();
+
+        return redirect()->to('/');
+    }
+}
